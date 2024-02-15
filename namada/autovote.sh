b8ee3c78cfd2e04c4b8d7d2191a2ef2db06cdae4
linamaruu <153367374+linamaruu@users.noreply.github.com>
2024-02-16 00:08:22 +0400

    Update autovote.sh

--- namada/autovote.sh
+++ namada/autovote.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Saving wallet address
+WALLET_ADDRESS=$(namadaw find --alias $WALLET | grep "Implicit" | awk '{print $3}') 
+echo "export WALLET_ADDRESS="$WALLET_ADDRESS"" >> $HOME/.bash_profile 
+source $HOME/.bash_profile
+
 # Writing the output
 prop_list=$(namadac query-proposal)
 
@@ -18,13 +23,10 @@ while read -r line; do
         # Extract Start Epoch
         start_epoch=$(echo "$line" | grep -oP 'Start Epoch: \K\d+')
 
-        # Extract Author
-        author_addr=$(echo "$line" | grep -oP 'Author: \K\S+')
-
         # Check if Start Epoch is equal to Last Committed Epoch + 1
         if [[ $start_epoch -eq $((last_committed_epoch + 1)) ]]; then
             # Vote using the sample command
-            namadac vote-proposal --proposal-id $prop_id --vote yay --address $author_addr --memo $MEMO
+            namadac vote-proposal --proposal-id $prop_id --vote yay --address $WALLET_ADDRESS --memo $MEMO
             echo "voted $prop_id"
         fi
 
