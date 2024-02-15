a774cfd13ce1c48c8ef56c5daed5d9ee465a5123
linamaruu <153367374+linamaruu@users.noreply.github.com>
2024-02-15 22:58:31 +0400

    Update autovote.sh

--- namada/autovote.sh
+++ namada/autovote.sh
@@ -1,1 +1,36 @@
+#!/bin/bash
 
+# Writing the output
+prop_list=$(namadac query-proposal)
+
+# Extract last committed epoch
+last_epoch=$(echo "$prop_list" | grep -oP 'Last committed epoch: \K\d+')
+
+# Extract ID
+id=$(echo "$prop_list" | grep -oP 'id: \K\d+')
+
+# Extract proposals and process each one
+while read -r line; do
+    if [[ $line =~ "Proposal Id: " ]]; then
+        # Extract proposal ID
+        prop_id=$(echo "$line" | grep -oP 'Proposal Id: \K\d+')
+
+        # Extract Start Epoch
+        start_epoch=$(echo "$line" | grep -oP 'Start Epoch: \K\d+')
+
+        # Extract Author
+        author_addr=$(echo "$line" | grep -oP 'Author: \K\S+')
+
+        # Check if Start Epoch is equal to Last Committed Epoch + 1
+        if [[ $start_epoch -eq $((last_committed_epoch + 1)) ]]; then
+            # Vote using the sample command
+            namadac vote-proposal --proposal-id $prop_id --vote yay --address $author_addr --memo $MEMO
+        fi
+
+        # Check if Prop ID equals ID, if so, end the script
+        if [[ $prop_id -eq $id ]]; then
+            echo "Reached last committed proposal. Exiting..."
+            exit 0
+        fi
+    fi
+done <<< "$output"
